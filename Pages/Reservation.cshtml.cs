76e172b47c5cd7d08817735efb05a6cf5aae205f
henz90 <59123400+henz90@users.noreply.github.com>
2020-09-01 19:54:52 -0400

    Trying to implement SQL
    Having Trouble Here

--- Pages/Reservation.cshtml.cs
+++ Pages/Reservation.cshtml.cs
@@ -19,7 +19,7 @@ namespace Hotel.Pages
         public DateTime Checkout {get; set;}
 
         [BindProperty, Required]
-        public int Room {get; set;}
+        public Room Room {get; set;}
 
         private readonly DatabaseContext db;  
         public ReservationModel(DatabaseContext db) => this.db = db;
@@ -29,25 +29,25 @@ namespace Hotel.Pages
         {
             ViewData["Checkin"] = Checkin;
             ViewData["Checkout"] = Checkout;
-            // Reservations = db.Reservations.Where(r => r.CheckIn.CompareTo(Checkin) >= 0  ;
             // query 1: find all reservations that exist where start OR end date is with the date range, then distinct(Room.Id) => list of room Ids that are occupied
-            Reservations = (List<Reservation>)db.Reservations.Where(r => r.CheckIn.CompareTo((DateTime)Checkin) <=0 && r.CheckOut.CompareTo((DateTime)Checkout) >=0); // FIXME: NEEDS WORK
+            // FIXME: NEEDS WORK
+            //.Where(r => r.CheckIn.CompareTo((DateTime)Checkin) >=0 && r.CheckOut.CompareTo((DateTime)Checkout) <=0)
+            //Reservations = db.Reservations.Where( ???? ).ToList();
             // query 2: find all rooms that do not have room.Id on the list created by query 1
-            Rooms = db.Rooms.ToList();
+            Rooms = db.Rooms.Where(r => r.RoomId.Equals(Reservations)).ToList();
         }
 
         public IActionResult OnPost(){
             if (ModelState.IsValid){
-                /*
                 Reservation reservation = new Reservation();
                 reservation.CheckIn = Checkin;
                 reservation.CheckOut = Checkout;
-                reservation.RoomId = Room;
+                reservation.Room = Room;
                 reservation.UserId = User.Identity.Name;
                 db.Add(reservation);
                 db.SaveChanges();
                 int id = reservation.ReservationId;
-                return RedirectToPage("ReservationSuccess", new {Checkin = Checkin.Date.ToString("MM-dd-yyyy"), Checkout = Checkout.Date.ToString("MM-dd-yyyy"), Room = reservation.RoomId, Id = id});    */
+                return RedirectToPage("ReservationSuccess", new {Checkin = Checkin.Date.ToString("MM-dd-yyyy"), Checkout = Checkout.Date.ToString("MM-dd-yyyy"), Room = reservation.Room.RoomId, Id = id});
             }
             return Page();
         }
