d094701312fc2d2d77ffa30cdc1b773879e3aa24
henz90 <59123400+henz90@users.noreply.github.com>
2020-09-02 10:15:26 -0400

    Getting Reservation completed

--- Pages/ReservationChange.cshtml.cs
+++ Pages/ReservationChange.cshtml.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Hotel.Data;
+using Hotel.Models;
 
 namespace Hotel.Pages
 {
     public class ReservationChangeModel : PageModel
     {
+        private DatabaseContext db;
+        public ReservationChangeModel(DatabaseContext db) => this.db = db;
+
+        [BindProperty(SupportsGet = true)]
+        public int Id { get; set; }
+
         public void OnGet()
         {
         }
