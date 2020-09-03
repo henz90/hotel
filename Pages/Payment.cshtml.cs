f821984bd19bcf9700c70abf72123d9f975d1dc9
zhaocmin <zhaocmin@gmail.com>
2020-09-03 12:16:19 -0400

    -- for test
    for test

--- Pages/Payment.cshtml.cs
+++ Pages/Payment.cshtml.cs
@@ -1,22 +1,37 @@
-using System.Security.Cryptography.X509Certificates;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using Stripe;
+using Hotel.Data;
+using Hotel.Models;
+using System.ComponentModel.DataAnnotations;
 namespace HotelProject.Pages
 {
     public class PaymentModel : PageModel
     {
-        public PaymentModel()
+        private readonly DatabaseContext db;
+        //public PaymentModel(DatabaseContext db) => 
+        public PaymentModel(DatabaseContext db)
         {
+            this.db = db;
             //PublicKey = config["Stripe:PublicKey"].ToString();   
             PublicKey = "pk_test_51HA2pUIjlJ1JmMzP0SL1QgxQLc3N4ME0X3D3ZiPZvN4VbT9xOWBJdrrjbpw1Zv36Xx7KNJ7LCtxLEQKLwHDrZQOw00bJLdtrxF";
         }
         public string PublicKey {get;}
+        public List<Reservation> Reservations { get; set; }
+        public List<Room> Rooms { get; set; }
+        public void OnGet()
+        {
+
+            var reservedRoomIds = db.Reservations.Where(r => r.UserId == User.Identity.Name).Select(r => r.Room.RoomId).ToHashSet();
+            Rooms = db.Rooms.Where(r => reservedRoomIds.Contains(r.RoomId)).ToList();
+            var customers = new CustomerService();
+        //Rooms = db.Rooms.Where(r => !reservedRoomIds.Contains(r.RoomId)).ToList();
+        }
+
         public IActionResult OnPost(string stripeEmail, string stripeToken)
         {
             var customers = new CustomerService();
