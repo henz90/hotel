3a0a03f43fda71c062f1a7e97e270672ea429d02
henz90 <59123400+henz90@users.noreply.github.com>
2020-09-04 10:24:18 -0400

    Updates

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -8,7 +8,6 @@ namespace Hotel.Models
     {
         public int ReservationId { get; set;}
         public Room Room { get; set; }
-        //public string UserId { get; set;}
         public IdentityUser User {get; set;}
         public DateTime CheckIn { get; set;}
         public DateTime CheckOut { get; set;}
